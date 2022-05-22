dd33128b8b6bcb2b5934faebdb2482f1dca1ef10
MartinGbz <martin.grabarz@outlook.fr>
2022-05-22 12:55:33 +0200

    trying to display messages in listView

--- app/src/main/java/com/example/chat_2022_eleves/ConversationActivity.kt
+++ app/src/main/java/com/example/chat_2022_eleves/ConversationActivity.kt
@@ -6,25 +6,36 @@ import android.util.Log
 import android.view.View
 import android.widget.*
 import com.google.gson.Gson
+import okhttp3.internal.notify
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
+
 
 class ConversationActivity : AppCompatActivity(), View.OnClickListener {
 
     var gs: GlobalState? = null
     var btnEnvoiMessage: Button? = null
     var champTxtMessage : EditText? = null
-    var svMessages: ScrollView? = null
+//    var svMessages: ScrollView? = null
+    var lvMessages: ListView? = null
     var hash: String? = null
     var conv: Conversation? = null
     var messages: ListMessages? = null
+    var arrayAdapter: Adapter? = null
+//    var list: Array<String?> = arrayOfNulls(0)
+//    var list: ArrayList<String?>? = arrayListOf(null)
+    var list = ArrayList<String>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_show_conversation)
         champTxtMessage = findViewById(R.id.conversation_edtMessage)
         btnEnvoiMessage = findViewById(R.id.conversation_btnOK)
+
+        lvMessages = findViewById(R.id.conversation_lvMessages)
+//        final messageAdapter = ArrayAdapter<String>(
+
         btnEnvoiMessage?.setOnClickListener(this)
         gs = application as GlobalState
         val bdl = this.intent.extras
@@ -34,10 +45,19 @@ class ConversationActivity : AppCompatActivity(), View.OnClickListener {
 
         getConvMessagesRequest()
 
+        arrayAdapter = ArrayAdapter<String>(
+            this,
+            android.R.layout.simple_list_item_1,
+            list
+        )
+        lvMessages?.adapter = arrayAdapter as ArrayAdapter<*>
+
         gs!!.alerter("data : " + (bdl?.getString("data") ?: ""))
         println(bdl)
 
-        svMessages = findViewById<View?>(R.id.conversation_svMessages) as ScrollView
+//        svMessages = findViewById<View?>(R.id.conversation_svMessages) as ScrollView
+
+
 
         // svMessages.
         // var obj = [{"id":"625"";contenu";"test";"auteur":"castor","couleur":"black"}]
@@ -69,8 +89,42 @@ class ConversationActivity : AppCompatActivity(), View.OnClickListener {
                 Log.i("get messages", res.toString())
                 if (res?.success.toBoolean()) {
                     messages = res
+                    val messagesList = arrayListOf<String>()
+                    for (m in messages?.getMessages()!!) {
+                        if (m != null) {
+                            messagesList.add(m.contenu!!)
+                        }
+                    }
+                    println(messagesList)
+//                    list = messagesList.toList().toTypedArray()
+                    list.clear();
+                    list.addAll(messagesList);
+//                    arrayAdapter?.notify()
+//                    notifyDataSetChanged();
+                    println(list)
+//                    list = messagesList
+
+//                    arrayAdapter.swapData(messagesList)
+//                    val a = ArrayAdapter(this, android.R.layout.simple_list_item_1, )
+//                    val adapter = ArrayAdapter(this, android.R.layout.simple_list_item_1, messagesList)
+//                    var a = messagesList.toArray() as Array<*>
+//                    var a = messagesList.toCollection(ArrayList())
+//                    createAdapter(messagesList)
                 }
             }
+
+//            fun swapData(filtersList: List<String?>?) {
+//                if (filtersList != null) {
+//                    this.arrayList.clear()
+//                    this.arrayList.addAll(filtersList)
+//                    notifyDataSetChanged()
+//                }
+//            }
+
+            private fun createAdapter(messagesList: ArrayList<String>) {
+
+            }
+
             override fun onFailure(call: Call<ListMessages?>?, t: Throwable?) {
                 call?.cancel()
             }
